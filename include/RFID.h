24c76b18c539e5e10d6f3474000fc3df91d0c32c
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-10-05 12:30:49 -0300

    Implementa leitura RFID

--- include/RFID.h
+++ include/RFID.h
@@ -1,12 +1,13 @@
 #ifndef __RFID_H__
 #define __RFID_H__
-#include "MFRC522.h";
+#include "MFRC522.h"
 
 class RFID {
     private:
         const int SS_PIN = 5;
         const int RST_PIN = 15;
         MFRC522 rfid;
+        String identificador;
     public:
         //=== Implementa padrão de projeto Singleton nessa classe
         RFID() = default;
@@ -18,7 +19,11 @@ class RFID {
 
         //=== Inicializa as configurações para o módulo funcionar.
         void inicializar(void);
-        // FIXME Método temporário que só exibe o id do RFID na saída padrão
+        //=== Método que tenta ler o RFID e coloca valor na variável identificador.
         void ler(void);
+        //=== Método que limpa identificador para próxima leitura.
+        void limparIdentificador(void);
+        //=== Obtém o identificador do RFID
+        String obterIdentificador(void) const;
 };
 #endif
--- src/RFID.cpp
+++ src/RFID.cpp
@@ -1,4 +1,4 @@
-#include "RFID.h";
+#include "RFID.h"
 
 void RFID::inicializar() {
     rfid = MFRC522{SS_PIN, RST_PIN};
@@ -9,17 +9,22 @@ void RFID::ler() {
     if (rfid.PICC_IsNewCardPresent()) { 
         if (rfid.PICC_ReadCardSerial()) {
             MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-            Serial.print("RFID/NFC Tag Type: ");
-            Serial.println(rfid.PICC_GetTypeName(piccType));
-            Serial.print("UID:");
+            Serial.print("| [RFID/NFC Tag Type]: ");
+            Serial.println(String(rfid.PICC_GetTypeName(piccType)) + " |");
             for (int i = 0; i < rfid.uid.size; i++) {
-                Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-                Serial.print(rfid.uid.uidByte[i], HEX);
+                identificador.concat(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                identificador.concat(String(rfid.uid.uidByte[i], HEX));
             }
-            Serial.println();
-
             rfid.PICC_HaltA(); // halt PICC
             rfid.PCD_StopCrypto1(); // stop encryption on PCD
         }
     }
+}
+
+String RFID::obterIdentificador() const {
+    return this->identificador;
+}
+
+void RFID::limparIdentificador() {
+    this->identificador = "";
 }
